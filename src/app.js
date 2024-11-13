3235976bcf705b06d25350adbdfb7d3aa5275c76
Sir-Vinicius <viniciusidai@gmail.com>
2024-11-13 16:47:17 -0300

    adicionando cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 require('dotenv').config();
+const cors = require('cors');
 
 const productRouter = require('./routes/productRoute')
 const userRouter = require('./routes/userRoute')
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use('/products', productRouter)
 app.use('/users', userRouter)

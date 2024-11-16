e11d983e80c77b2f37247e5225e59264da7b1146
darlison-calm <bernardo.darlison@gmail.com>
2024-11-15 22:23:07 -0300

    fix: token no post de categorias

--- src/routes/categoryRoute.js
+++ src/routes/categoryRoute.js
@@ -1,11 +1,12 @@
 const categoryController = require('../controllers/categoryControllers');
 const { authenticateToken } = require('../middlewares/userMiddleware');
+const { authenticateUser } = require('../services/userServices');
 
 const categoryRouter = require('express').Router();
 
 categoryRouter.get('/:id', categoryController.getById);
 categoryRouter.get('/', categoryController.getAll);
-categoryRouter.post('/', categoryController.create);
+categoryRouter.post('/', authenticateToken, categoryController.create);
 categoryRouter.delete('/:id', authenticateToken, categoryController.deleteCategory)
 categoryRouter.put('/:id', authenticateToken, categoryController.updateCategory)
 module.exports = categoryRouter;

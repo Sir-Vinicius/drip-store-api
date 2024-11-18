8da4870095e7e62db232757236a6678d573cc422
darlison-calm <bernardo.darlison@gmail.com>
2024-11-18 13:18:06 -0300

    fix: users login mensagem de error

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -72,9 +72,9 @@ const login = async (req, res) => {
 
   } catch (error) {
     if (error.message === 'Usuário não encontrado') {
-      return res.status(404).json({ error: 'Usuário não encontrado' });
+      return res.status(404).json({ error: error.message });
     } else if (error.message === 'Email ou senha incorretos') {
-      return res.status(401).json({ error: 'Email ou senha incorretos' });
+      return res.status(401).json({ error: error.message });
     }
     res.status(500).json({ error: 'Erro interno do servidor' });
   }
